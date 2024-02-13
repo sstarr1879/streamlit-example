cd86b36f2244a43d06222850bb4b1dc828eda496
Sarah Starr <sarahstarr@Sarahs-Air-2.localdomain>
2024-02-12 19:04:12 -0500

    commit1

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import the Streamlit library
+import streamlit as st
+
+# Set the title of the app
+st.title('Welcome to My Streamlit App')
+
+# Ask the user for their name
+# name = st.text_input('What is your name?')
+
+# When the user inputs their name and presses Enter, greet them
+st.write('Hello!')
+
+# Add some additional text
+st.write("This is a simple example of a Streamlit app for IDSG. You can extend this app with more Streamlit widgets to interact with your users.")
